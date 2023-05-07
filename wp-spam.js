523160f6bec09229c2a4f1b8c479e19cac864443
AHN <ahndo7059@gmail.com>
2023-05-07 22:27:52 +0600

    Added error handling

--- wp-spam.js
+++ wp-spam.js
@@ -1,17 +1,45 @@
-setInterval(() => {
+// setInterval(() => {
 
-    const message = "Nevermind";
+//     const message = "Message"; //message
 
-    const area = document.querySelector('#main div[contenteditable="true"]');
+//     const area = document.querySelector('#main div[contenteditable="true"]');
 
-    area.focus();
-    document.execCommand('insertText', false, message)
-    dispatchEvent(new Event('change', { bubbles: true }))
+//     if (area) {
 
-    setTimeout(() => {
-        let btn = document.querySelector('#main [data-testid="send"]');
+//         area.focus();
+//         document.execCommand('insertText', false, message)
+//         dispatchEvent(new Event('change', { bubbles: true }))
+//         setTimeout(() => {
+//                 let btn = document.querySelector('#main [data-testid="send"]');
+//                 btn.click();
+//             }, 100)
 
-        btn.click();
-    }, 100)
 
-}, 500)
+//     }else if(!area){
+//         alert("Select a chat")
+//     }
+
+// }, 500) //after every 500ms
+
+
+
+const message = "Message"; //message
+
+const area = document.querySelector('#main div[contenteditable="true"]');
+
+if (area) {
+
+    setInterval(() => {
+        area.focus();
+        document.execCommand('insertText', false, message)
+        dispatchEvent(new Event('change', { bubbles: true }))
+        setTimeout(() => {
+            let btn = document.querySelector('#main [data-testid="send"]');
+            btn.click();
+        }, 100)
+    }, 500); //after every 500ms
+
+
+} else {
+    alert("Select a chat")
+}
